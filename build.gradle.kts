fba1e149cdf549758b798cc9f189115ad74dc25c
Ainur Abdrashitov <smithunknownslv@gmail.com>
2022-05-26 02:33:46 +0300

    Set up server with API

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,21 @@ plugins {
 group = "io.aabdrashitov"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core:2.0.1")
+    implementation("io.ktor:ktor-server-netty:2.0.1")
+    implementation("io.ktor:ktor-server-content-negotiation:2.0.1")
+    implementation("io.ktor:ktor-serialization-jackson:2.0.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
     testImplementation(kotlin("test"))
 }
 
